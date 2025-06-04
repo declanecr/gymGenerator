772aa94137fa6c3454e743ed2d5cd1ed51bc7cb3
declanecr <declan.cr04@gmail.com>
2025-06-04 13:26:58 -0400

    feat(test): update jest/ts config files to properly implement tests
    - updated test.jes-e2e.json to map  aliases correctly
    - tweaked tsconfig.json to support E2E compilation
    - applied related chagnes in package.json and package-lock.json
    
    Ref #12

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,11 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { App } from 'supertest/types';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,9 +15,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /hello', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/hello')
       .expect(200)
       .expect('Hello World!');
   });
