13879876af064baf108910c521880393d2887654
declanecr <declan.cr04@gmail.com>
2025-06-19 13:23:55 -0400

    feat(api): added QueryClient to entire app by wrapping the app in a QueryClientProvider. Closes #10

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthProvider } from './context/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 )
