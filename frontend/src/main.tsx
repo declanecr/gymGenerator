6e502a1d806270944c3b52339c909b91a272cbe2
declanecr <declan.cr04@gmail.com>
2025-07-28 17:27:49 -0400

    feat(theme): create shared theme file
    uses UC color scheme
    has dark mode
    can control typography:
    - font family
    - weight scale
    - header sizing
    - button casing (default 'textTransform' removed)
    accesible focus styles for buttons and inputs
    
    Closes #33

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,20 @@ import './index.css'
 import App from './App'
 import { AuthProvider } from './context/AuthProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { getTheme } from './theme'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </AuthProvider>
+    <ThemeProvider theme={getTheme()}>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline />
+          <App />
+        </QueryClientProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
